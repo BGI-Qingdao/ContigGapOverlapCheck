2e646f6f7a17b1f32087f59490d1989c36cebea1
Lidong Guo <guolidong@genomics.cn>
2018-12-05 14:32:00 +0800

    bug of use sort command

--- quast_analysis/order_contig.sh
+++ quast_analysis/order_contig.sh
@@ -4,7 +4,7 @@ echo "use quast file [ $1 ]"
 ## check contig info 
 awk -f quast_contig_classify.awk < $1
 ## check 2 neib contig info 
-cat unique_contig.tsv | awk '{ if(NR%2==0){print $0} }' | sort -k 5 -kn 1  >sorted_unique_contig.txt
+cat unique_contig.tsv | awk '{ if(NR%2==0){print $0} }' | sort -k5,5 -k1,1n  >sorted_unique_contig.txt
 awk -f overlap_detect.awk <sorted_unique_contig.txt
 ## print hist
 cat overlap_info.txt | awk '{print $18}' | sort | uniq -c |  sort -nk 2 >overlap_hist.txt
