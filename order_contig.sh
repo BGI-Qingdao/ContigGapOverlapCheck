b27dcc4552ad1b634d9d5c4cc8d61f7973c68f43
Guo Lidong <guolidong@genomics.cn>
2019-09-25 15:27:25 +0800

    use new scripts

--- order_contig.sh
+++ order_contig.sh
@@ -2,7 +2,7 @@
 
 echo "use quast file [ $1 ]"
 ## check contig info 
-awk -f quast_contig_classify.awk < $1
+awk -f quast_contig_classify_new.awk < $1
 ## check 2 neib contig info 
 cat unique_contig.tsv | awk '{ if(NR%2==0){print $0} }' | sort -k5,5 -k1,1n  >sorted_unique_contig.txt
 awk -f overlap_detect.awk <sorted_unique_contig.txt
